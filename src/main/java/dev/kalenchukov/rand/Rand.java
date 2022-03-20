889da4034c0be2037bafdd108381f0f4cf5272c8
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-03-20 10:41:13 +0300

    Добавлено описание методов и класса

--- src/main/java/dev/kalenchukov/rand/Rand.java
+++ src/main/java/dev/kalenchukov/rand/Rand.java
@@ -9,34 +9,49 @@ package dev.kalenchukov.rand;
 import org.apache.commons.lang3.ArrayUtils;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Класс содержит статические методы для получения строк случайных символов.
+ */
 public final class Rand
 {
+	/**
+	 * Набор десятичных цифр.
+	 */
 	private static final char[] DIGIT = {
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 	};
 
+	/**
+	 * Набор прописных букв латинского алфавита.
+	 */
 	private static final char[] LOWER = {
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 	};
 
+	/**
+	 * Набор строчных букв латинского алфавита.
+	 */
 	private static final char[] UPPER = {
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 	};
 
+	/**
+	 * Набор символов печатаемых с клавиатуры
+	 */
 	private static final char[] GRAPH = {
 		'!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':',
 		';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'
 	};
 
-	private Rand()
-	{
-
-	}
+	private Rand() {}
 
 	/**
-	 * Возвращает строку из символов [0-9]
+	 * Возвращает строку из символов [0-9].
+	 *
+	 * @param length Количество символов в возвращаемой строке.
+	 * @return Строку со случайным порядком символов.
 	 */
 	@NotNull
 	public static String digit(int length)
@@ -45,7 +60,10 @@ public final class Rand
 	}
 
 	/**
-	 * Возвращает строку из символов [a-z]
+	 * Возвращает строку из символов [a-z].
+	 *
+	 * @param length Количество символов в возвращаемой строке.
+	 * @return Строку со случайным порядком символов.
 	 */
 	@NotNull
 	public static String lower(int length)
@@ -54,7 +72,10 @@ public final class Rand
 	}
 
 	/**
-	 * Возвращает строку из символов [A-Z]
+	 * Возвращает строку из символов [A-Z].
+	 *
+	 * @param length Количество символов в возвращаемой строке.
+	 * @return Строку со случайным порядком символов.
 	 */
 	@NotNull
 	public static String upper(int length)
@@ -63,7 +84,10 @@ public final class Rand
 	}
 
 	/**
-	 * Возвращает строку из символов [A-Za-z]
+	 * Возвращает строку из символов [A-Za-z].
+	 *
+	 * @param length Количество символов в возвращаемой строке.
+	 * @return Строку со случайным порядком символов.
 	 */
 	@NotNull
 	public static String alpha(int length)
@@ -72,7 +96,10 @@ public final class Rand
 	}
 
 	/**
-	 * Возвращает строку из символов [A-Za-z0-9]
+	 * Возвращает строку из символов [A-Za-z0-9].
+	 *
+	 * @param length Количество символов в возвращаемой строке.
+	 * @return Строку со случайным порядком символов.
 	 */
 	@NotNull
 	public static String alnum(int length)
@@ -86,7 +113,10 @@ public final class Rand
 	}
 
 	/**
-	 * Возвращает строку из символов [a-zA-Z0-9!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]
+	 * Возвращает строку из символов [a-zA-Z0-9!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~].
+	 *
+	 * @param length Количество символов в возвращаемой строке.
+	 * @return Строку со случайным порядком символов.
 	 */
 	@NotNull
 	public static String graph(int length)
@@ -98,6 +128,13 @@ public final class Rand
 		);
 	}
 
+	/**
+	 * Возвращает строку из указанного набора символов.
+	 *
+	 * @param length Количество символов в возвращаемой строке.
+	 * @param symbols Набор символов из которых должна состоять строка.
+	 * @return Строку со случайным порядком символов.
+	 */
 	@NotNull
 	private static String generate(int length, char[] symbols)
 	{
