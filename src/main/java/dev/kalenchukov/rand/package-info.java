0f30f1383b45a1710b924daaf98c6431eb588af4
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-03-20 10:09:42 +0300

    Обновлён копирайт

--- src/main/java/dev/kalenchukov/rand/package-info.java
+++ src/main/java/dev/kalenchukov/rand/package-info.java
@@ -1,10 +1,7 @@
-/**
- * Генератор строк случайных символов
- *
- * @author <a href="mailto:aleksey.kalenchukov@yandex.ru">Алексей Каленчуков</a>
- * @copyright © 2021 Алексей Каленчуков
- * @package Rand
- * @version 1.1.0
+/*
+ * Copyright © 2022 Алексей Каленчуков
+ * GitHub: https://github.com/kalenchukov
+ * E-mail: mailto:aleksey.kalenchukov@yandex.ru
  */
 
 package dev.kalenchukov.rand;
