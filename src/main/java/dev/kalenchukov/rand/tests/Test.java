058657199dd33dbbd12c2514c1c466db8539a98c
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-03-20 10:13:20 +0300

    Добавлена папка для тестовых запусков

---
+++ src/main/java/dev/kalenchukov/rand/tests/Test.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2022 Алексей Каленчуков
+ * GitHub: https://github.com/kalenchukov
+ * E-mail: mailto:aleksey.kalenchukov@yandex.ru
+ */
+
+package dev.kalenchukov.rand.tests;
+
+import dev.kalenchukov.rand.Rand;
+
+public final class Test
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Rand.digit(8));
+
+		System.out.println(Rand.lower(16));
+
+		System.out.println(Rand.upper(24));
+
+		System.out.println(Rand.alpha(32));
+
+		System.out.println(Rand.alnum(40));
+
+		System.out.println(Rand.graph(48));
+	}
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module dev.kalenchukov.rand
+{
+	requires org.jetbrains.annotations;
+	requires commons.lang3;
+
+	exports dev.kalenchukov.rand;
+}
