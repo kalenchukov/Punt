5fc6f63e1141e1cb522271756932435e4091f39a
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-03-20 10:02:51 +0300

    Изменена версия

--- src/main/java/dev/kalenchukov/rand/package-info.java
+++ src/main/java/dev/kalenchukov/rand/package-info.java
@@ -4,7 +4,7 @@
  * @author <a href="mailto:aleksey.kalenchukov@yandex.ru">Алексей Каленчуков</a>
  * @copyright © 2021 Алексей Каленчуков
  * @package Rand
- * @version 1.0.0
+ * @version 1.1.0
  */
 
 package dev.kalenchukov.rand;
