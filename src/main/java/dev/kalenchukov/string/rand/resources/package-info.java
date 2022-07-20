6ba990f021115f98ca1d312dbabb15905e6cbd09
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-07-20 21:46:16 +0300

    Удалён пакет resources

--- src/main/java/dev/kalenchukov/string/rand/resources/package-info.java
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright © 2022 Алексей Каленчуков
- * GitHub: https://github.com/kalenchukov
- * E-mail: mailto:aleksey.kalenchukov@yandex.ru
- */
-
-package dev.kalenchukov.string.rand.resources;
